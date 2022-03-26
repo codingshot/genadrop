68fdc2ca0825e9f3065e371f8d1254898740c951
Ali Alrubaye <alialrubaye1990@hotmail.com>
2022-03-26 15:52:46 -0400

    refactored files in root dir

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, lazy, Suspense, useState } from 'react';
-import { Switch, Route } from "react-router-dom";
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/footer';
 import Navbar from './components/Navbar/Navbar';
@@ -8,7 +7,6 @@ import Overlay from './components/overlay/overlay';
 import { fetchCollections, readAllSingleNft } from './utils/firebase';
 import { GenContext } from './gen-state/gen.context';
 import { setCollections, setSingleNfts } from './gen-state/gen.actions';
-// import Fallback from './pages/fallback/fallback';
 import Notification from './components/Notification/Notification';
 import Clipboard from './components/clipboard/clipboard';
 import Loader from './components/Loader/Loader';
@@ -27,10 +25,11 @@ const Collections = lazy(() => import('./pages/collections/collections'));
 const Dashboard = lazy(() => import('./pages/dashboard/dashboard'));
 const List = lazy(() => import('./pages/listNFT/list'));
 const Profile = lazy(() => import('./pages/profile/profile'));
-const SingleNftCollection = lazy(() => import('./pages/singleMintCollection/singleNftCollection'));
+const SingleNftCollection = lazy(() =>
+  import('./pages/singleMintCollection/singleNftCollection')
+);
 const SingleNFT = lazy(() => import('./pages/singleNFT/singleNFT'));
-const Listed = lazy(() => import('./pages/listNFT/listed'))
-// import Listed from './pages/listNFT/listed'
+const Listed = lazy(() => import('./pages/listNFT/listed'));
 
 function App() {
   const { dispatch } = useContext(GenContext);
@@ -39,52 +38,73 @@ function App() {
   useEffect(() => {
     (async function getCollections() {
       let collections = await fetchCollections();
-      dispatch(setCollections(collections))
-    }());
+      dispatch(setCollections(collections));
+    })();
 
     (async function readAllSingle() {
       let singleNfts = await readAllSingleNft();
-      dispatch(setSingleNfts(singleNfts))
-    }());
+      dispatch(setSingleNfts(singleNfts));
+    })();
   }, []);
 
   if (showWelcomeScreen) {
-    return <Welcome showWelcomeScreen={setShowWelcomeScreen} />
-  } else {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="Routes">
-          <Switch>
-            <ErrorBoundary>
-              <Suspense fallback={<Loading />}>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/create" component={Create} />
-                <Route exact path="/preview" component={Preview} />
-                <Route exact path="/mint" component={Mint} />
-                <Route exact path="/marketplace" component={Marketplace} />
-                <Route exact path="/marketplace/single-mint" component={SingleNftCollection} />  
-                <Route exact path="/marketplace/single-mint/:nftId" component={SingleNFT} />
-                <Route exact path="/marketplace/collections" component={Collections} />
-                <Route exact path="/marketplace/collections/:collectionName" component={Explore} />
-                <Route exact path="/marketplace/collections/:collectionName/:nftId" component={CollectionNFT} />
-                <Route exact path="/me/:userId" component={Dashboard} />
-                <Route exact path="/me/:userId/:nftId" component={List} />
-                <Route exact path="/me/:userId/profile/settings" component={Profile} />
-                {/* <Route path="" component={Fallback} /> */}
-              </Suspense>
-            </ErrorBoundary>
-          </Switch>
-        </div>
-        <Footer />
-
-        <Overlay />
-        <Notification />
-        <Clipboard />
-        <Loader />
-      </div>
-    )
+    return <Welcome showWelcomeScreen={setShowWelcomeScreen} />;
   }
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="Routes">
+        <Switch>
+          <ErrorBoundary>
+            <Suspense fallback={<Loading />}>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/create" component={Create} />
+              <Route exact path="/preview" component={Preview} />
+              <Route exact path="/mint" component={Mint} />
+              <Route exact path="/marketplace" component={Marketplace} />
+              <Route
+                exact
+                path="/marketplace/single-mint"
+                component={SingleNftCollection}
+              />
+              <Route
+                exact
+                path="/marketplace/single-mint/:nftId"
+                component={SingleNFT}
+              />
+              <Route
+                exact
+                path="/marketplace/collections"
+                component={Collections}
+              />
+              <Route
+                exact
+                path="/marketplace/collections/:collectionName"
+                component={Explore}
+              />
+              <Route
+                exact
+                path="/marketplace/collections/:collectionName/:nftId"
+                component={CollectionNFT}
+              />
+              <Route exact path="/me/:userId" component={Dashboard} />
+              <Route exact path="/me/:userId/:nftId" component={List} />
+              <Route
+                exact
+                path="/me/:userId/profile/settings"
+                component={Profile}
+              />
+            </Suspense>
+          </ErrorBoundary>
+        </Switch>
+      </div>
+      <Footer />
+      <Overlay />
+      <Notification />
+      <Clipboard />
+      <Loader />
+    </div>
+  );
 }
 
 export default App;
