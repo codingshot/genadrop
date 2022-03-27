782fab7fef4852c08f59e60cf86385386eee0b98
Ebube111 <ebubeagwaze@gmail.com>
2022-03-27 03:19:06 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, lazy, Suspense, useState } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, Router } from "react-router-dom";
 
 import "./App.css";
 import Footer from "./components/footer/footer";
@@ -13,6 +13,7 @@ import Clipboard from "./components/clipboard/clipboard";
 import Loader from "./components/Loader/Loader";
 import ErrorBoundary from "./components/error-boundary/error-boundary";
 import Loading from "./pages/loading/loading";
+// import Fallback from "./pages/fallback/fallback";
 import Welcome from "./pages/welcome/welcome";
 
 const Home = lazy(() => import("./pages/home/home"));
@@ -57,51 +58,48 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="Routes">
-          <Switch>
-            <ErrorBoundary>
-              <Suspense fallback={<Loading />}>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/create" component={Create} />
-                <Route exact path="/preview" component={Preview} />
-                <Route exact path="/mint" component={Mint} />
-                <Route exact path="/marketplace" component={Marketplace} />
-                <Route
-                  exact
-                  path="/marketplace/single-mint"
-                  component={SingleNftCollection}
-                />
-                <Route
-                  exact
-                  path="/marketplace/single-mint/:nftId"
-                  component={SingleNFT}
-                />
-                <Route
-                  exact
-                  path="/marketplace/collections"
-                  component={Collections}
-                />
-                <Route
-                  exact
-                  path="/marketplace/collections/:collectionName"
-                  component={Explore}
-                />
-                <Route
-                  exact
-                  path="/marketplace/collections/:collectionName/:nftId"
-                  component={CollectionNFT}
-                />
-                <Route exact path="/me/:userId" component={Dashboard} />
-                <Route exact path="/me/:userId/:nftId" component={List} />
-                <Route
-                  exact
-                  path="/me/:userId/profile/settings"
-                  component={Profile}
-                />
-                <Route exact path="/404" component={Fallback} />
-                <Redirect to="/404" />
-              </Suspense>
-            </ErrorBoundary>
-          </Switch>
+          <ErrorBoundary>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/create" component={Create} />
+              <Route exact path="/preview" component={Preview} />
+              <Route exact path="/mint" component={Mint} />
+              <Route exact path="/marketplace" component={Marketplace} />
+              <Route
+                exact
+                path="/marketplace/single-mint"
+                component={SingleNftCollection}
+              />
+              <Route
+                exact
+                path="/marketplace/single-mint/:nftId"
+                component={SingleNFT}
+              />
+              <Route
+                exact
+                path="/marketplace/collections"
+                component={Collections}
+              />
+              <Route
+                exact
+                path="/marketplace/collections/:collectionName"
+                component={Explore}
+              />
+              <Route
+                exact
+                path="/marketplace/collections/:collectionName/:nftId"
+                component={CollectionNFT}
+              />
+              <Route exact path="/me/:userId" component={Dashboard} />
+              <Route exact path="/me/:userId/:nftId" component={List} />
+              <Route
+                exact
+                path="/me/:userId/profile/settings"
+                component={Profile}
+              />
+              <Route component={Fallback} />
+            </Switch>
+          </ErrorBoundary>
         </div>
         <Footer />
         <Overlay />
