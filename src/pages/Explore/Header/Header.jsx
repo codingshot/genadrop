7830886d7d8863a1ee2be152d03cb073a604dc61
weber77 <weberkanga77@gmail.com>
2022-03-29 15:51:52 +0100

    fix: NaN Algo price

--- src/pages/Explore/Header/Header.jsx
+++ src/pages/Explore/Header/Header.jsx
@@ -19,12 +19,15 @@ const Header = ({ collection, getHeight }) => {
   const handleSetState = payload => {
     setState(state => ({ ...state, ...payload }))
   }
-
   useEffect(() => {
     axios.get(`https://api.coinbase.com/v2/prices/ALGO-USD/spot`)
       .then(res => {
         handleSetState({ algoPrice: res.data.data.amount * price })
       })
+  }, [price])
+
+  useEffect(() => {
+
     document.documentElement.scrollTop = 0;
 
     window.addEventListener("resize", e => {
@@ -75,7 +78,7 @@ const Header = ({ collection, getHeight }) => {
         <div className={classes.detailContentWrapper}>
           <div className={classes.floorPrice}>
             <div className={classes.floor}>FLOOR PRICE</div>
-            <div className={classes.price}>{price} <span className={classes.chain}>Algo</span>  <span className={classes.usdPrice}>({algoPrice.toFixed(2)} USD)</span></div>
+            <div className={classes.price}>{price} <span className={classes.chain}>Algo</span> {price ? <span className={classes.usdPrice}>({algoPrice.toFixed(2)} USD)</span> : ""}</div>
           </div>
           <img src={stackIcon} alt="" />
         </div>
