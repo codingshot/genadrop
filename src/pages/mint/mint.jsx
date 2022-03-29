0e7d19691b6d6003186201980fae55e89cdf7d24
Ebube111 <ebubeagwaze@gmail.com>
2022-03-29 22:32:15 +0100

    changes

--- src/pages/mint/mint.jsx
+++ src/pages/mint/mint.jsx
@@ -1,103 +1,141 @@
-import classes from './mint.module.css';
-import { useRef, useState, useEffect, useContext } from 'react';
-import { handleZipFile } from './mint-script';
-import AssetPreview from '../../components/Mint/AssetPreview/AssetPreview';
-import lineIcon from '../../assets/icon-line.svg';
-import { GenContext } from '../../gen-state/gen.context';
+import classes from "./mint.module.css";
+import { useRef, useState, useEffect, useContext } from "react";
+import { handleZipFile } from "./mint-script";
+import AssetPreview from "../../components/Mint/AssetPreview/AssetPreview";
+import lineIcon from "../../assets/icon-line.svg";
+import Popup from "reactjs-popup";
+import { GenContext } from "../../gen-state/gen.context";
 
 const Mint = () => {
-
   const fileRef = useRef(null);
   const dropRef = useRef(null);
 
   const [state, setState] = useState({
-    fileName: '',
+    fileName: "",
     file: null,
     metadata: null,
-    zip: null
+    zip: null,
   });
 
   const { fileName, file, metadata, zip } = state;
 
-  const handleSetState = payload => {
-    setState(state => ({ ...state, ...payload }))
-  }
+  const handleSetState = (payload) => {
+    setState((state) => ({ ...state, ...payload }));
+  };
 
-  const handleFileChange = event => {
+  const handleFileChange = (event) => {
     let file = event.target.files[0];
     if (!file) return;
 
-    let name = file.name.split('.')
+    let name = file.name.split(".");
     let fileName = name[0];
     let fileType = name[1];
-    let supportedTypes = ['zip', 'png', 'jpeg', 'jpg', 'webp'];
+    let supportedTypes = ["zip", "png", "jpeg", "jpg", "webp"];
     if (!supportedTypes.includes(fileType.toLowerCase())) return;
 
-    if (fileType === 'zip') {
-      handleSetState({ zip: file, fileName })
+    if (fileType === "zip") {
+      handleSetState({ zip: file, fileName });
       handleZipFile({ file, handleSetState });
     } else {
       handleSetState({ file: [file], fileName });
     }
-  }
+  };
 
   useEffect(() => {
-    if (!dropRef.current) return
-    dropRef.current.ondragover = e => {
+    if (!dropRef.current) return;
+    dropRef.current.ondragover = (e) => {
       e.preventDefault();
-      document.querySelector('#drop-area').style.border = '2px dashed green'
-    }
-    dropRef.current.ondragleave = e => {
+      document.querySelector("#drop-area").style.border = "2px dashed green";
+    };
+    dropRef.current.ondragleave = (e) => {
       e.preventDefault();
-      document.querySelector('#drop-area').style.border = '1px dashed gainsboro'
-    }
-    dropRef.current.ondrop = e => {
+      document.querySelector("#drop-area").style.border =
+        "1px dashed gainsboro";
+    };
+    dropRef.current.ondrop = (e) => {
       e.preventDefault();
-      document.querySelector('#drop-area').style.border = '1px dashed gainsboro'
+      document.querySelector("#drop-area").style.border =
+        "1px dashed gainsboro";
       handleFileChange({ target: e.dataTransfer });
-    }
-
+    };
   }, [file]);
-
-
 
   return (
     <div className={classes.container}>
-      {
-        file ?
-          <AssetPreview
-            data={{ file, fileName, metadata, zip }}
-            changeFile={() => handleSetState({
-              fileName: '',
+      {file ? (
+        <AssetPreview
+          data={{ file, fileName, metadata, zip }}
+          changeFile={() =>
+            handleSetState({
+              fileName: "",
               file: null,
               metadata: null,
-              zip: null
-            })} />
-          :
-          <div className={classes.wrapper}>
-            <h1 className={classes.title}>Mint Your NFTs</h1>
-            <p className={classes.description}>
-              Upload an <span> <img src={lineIcon} alt="" /> asset</span> or a <span> <img src={lineIcon} alt="" /> collection</span> to create NFT
-            </p>
-            <div ref={dropRef} className={classes.uploadWrapper}>
-              <div>
-                <p id='drop-area' className={classes.dropArea}>Drag and Drop you files</p>
-                <p>Supported file types: zip, png, jpeg, jpg, webp</p>
+              zip: null,
+            })
+          }
+        />
+      ) : (
+        <div className={classes.wrapper}>
+          <h1 className={classes.title}>Mint Your NFTs</h1>
+          <p className={classes.description}>
+            Upload an{" "}
+            <Popup
+              position="bottom center"
+              on={["hover", "focus", "click"]}
+              trigger={
+                <span>
+                  {" "}
+                  <img src={lineIcon} alt="" /> asset
+                </span>
+              }
+            >
+              <div className={classes["tooltip"]}>
+                An asset is an individual layer for your generative art in .PNG
+                format (rendered as the same dimensions as your other assets)
               </div>
-              <p>or</p>
-              <button onClick={() => fileRef.current.click()}>Browse files</button>
-              <input
-                style={{ display: 'none' }}
-                onChange={handleFileChange}
-                ref={fileRef}
-                type="file"
-                accept=".jpg, .jpeg, .png, .webp, .zip"
-              />
+            </Popup>{" "}
+            or a{" "}
+            <Popup
+              position="bottom center"
+              on={["hover", "focus", "click"]}
+              trigger={
+                <span>
+                  {" "}
+                  <img src={lineIcon} alt="" /> collection
+                </span>
+              }
+            >
+              <div className={classes["tooltip"]}>
+                A collection is a rendered batch of generative images that the
+                GenaDrop create app has downloaded as a .ZIP folder with
+                accompanying metadata and all individual images as, PNGs
+              </div>
+            </Popup>
+            to create NFT
+          </p>
+          <div ref={dropRef} className={classes.uploadWrapper}>
+            <div>
+              <p id="drop-area" className={classes.dropArea}>
+                Drag and Drop you files
+              </p>
+              <p>Supported file types: zip, png, jpeg, jpg, webp</p>
             </div>
+            <p>or</p>
+            <button onClick={() => fileRef.current.click()}>
+              Browse files
+            </button>
+            <input
+              style={{ display: "none" }}
+              onChange={handleFileChange}
+              ref={fileRef}
+              type="file"
+              accept=".jpg, .jpeg, .png, .webp, .zip"
+            />
           </div>
-      }
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Mint;
