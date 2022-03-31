60e63db003e9649a6905251efaa6be35c20134a5
M-Rb3 <rabea9220@gmail.com>
2022-03-31 17:40:06 +0200

    add url query

--- src/components/Marketplace/Search-bar/searchBar.component.jsx
+++ src/components/Marketplace/Search-bar/searchBar.component.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import classes from './searchBar.module.css';
 
 const SearchBar = ({ onSearch }) => {
+  const history = useHistory();
+  const location = useLocation();
+
   const [state, setState] = useState({
     searchValue: '',
   });
@@ -9,18 +13,31 @@ const SearchBar = ({ onSearch }) => {
   const { searchValue } = state;
 
   const handleSetState = (payload) => {
-    setState((state) => ({ ...state, ...payload }));
+    setState((states) => ({ ...states, ...payload }));
   };
-
+  useEffect(() => {
+    const { search } = location;
+    const name = new URLSearchParams(search).get('search');
+    if (name) {
+      onSearch(name);
+      console.log(name);
+      handleSetState({ searchValue: name });
+    }
+  }, []);
   useEffect(() => {
     onSearch(searchValue);
   }, [searchValue]);
 
+  const seachHandler = (event) => {
+    const params = new URLSearchParams({ search: event.target.value });
+    history.replace({ pathname: location.pathname, search: params.toString() });
+    handleSetState({ searchValue: event.target.value });
+  };
   return (
     <input
       className={classes.searchInput}
       type="search"
-      onChange={(event) => handleSetState({ searchValue: event.target.value })}
+      onChange={seachHandler}
       value={searchValue}
       placeholder="search"
     />
