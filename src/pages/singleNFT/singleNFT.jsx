d7967cada4c5125ce7759abf08c8390c288a2eb3
M-Rb3 <rabea9220@gmail.com>
2022-04-07 16:21:28 +0200

    no attributes NFT display page fix

--- src/pages/singleNFT/singleNFT.jsx
+++ src/pages/singleNFT/singleNFT.jsx
@@ -166,12 +166,18 @@ const SingleNFT = () => {
 
   const attributeContent = () => (
     <div className={classes.attributesContainer}>
-      {nftDetails.properties.map((attribute, idx) => (
-        <div key={idx} className={classes.attribute}>
-          <span className={classes.title}>{attribute.trait_type}</span>
-          <span className={classes.description}>{attribute.value}</span>
-        </div>
-      ))}
+      {nftDetails.properties.map((attribute, idx) => {
+        return attribute.trait_type && attribute.value ? (
+          <div key={idx} className={classes.attribute}>
+            <span className={classes.title}>{attribute.trait_type}</span>
+            <span className={classes.description}>{attribute.value}</span>
+          </div>
+        ) : nftDetails.properties.length === 1 ? (
+          <span> No Attributes Available</span>
+        ) : (
+          <></>
+        );
+      })}
     </div>
   );
 
@@ -186,7 +192,6 @@ const SingleNFT = () => {
     // eslint-disable-next-line no-alert
     alert(res);
   };
-
   return (
     <div className={classes.container}>
       <div className={classes.section1}>
