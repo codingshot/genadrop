a1c18002634a6c85a9ec65f13cb6d263a942875f
EECvision <eec.studies@gmail.com>
2022-05-04 15:32:36 +0100

    add create page explainer video

---
+++ src/components/create-guide/create-guide-script.js
@@ -0,0 +1,33 @@
+import vid1 from "../../assets/vid/1Add_Layers.webm";
+import vid2 from "../../assets/vid/1Upload_images.webm";
+import vid3 from "../../assets/vid/1Re-order Layers.webm";
+import vid4 from "../../assets/vid/1Set conflict rule.webm";
+import vid5 from "../../assets/vid/set-number.webm";
+import vid6 from "../../assets/vid/1Generate.webm";
+
+export const guide = {
+  1: {
+    vid: vid1,
+    title: "Add layers",
+  },
+  2: {
+    vid: vid2,
+    title: "Upload images",
+  },
+  3: {
+    vid: vid3,
+    title: "Re-order layers",
+  },
+  4: {
+    vid: vid4,
+    title: "Set conflict rules",
+  },
+  5: {
+    vid: vid5,
+    title: "Input number of arts to generate",
+  },
+  6: {
+    vid: vid6,
+    title: "Generate, preview and download arts",
+  },
+};
