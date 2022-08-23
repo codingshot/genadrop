8442f585754d307d116b4a829fc463c99626b855
M-Rb3 <rabea9220@gmail.com>
2022-08-23 18:22:14 +0200

    fix page overlay

--- src/pages/links/links.jsx
+++ src/pages/links/links.jsx
@@ -24,40 +24,42 @@ const Links = () => {
     });
   };
   return (
-    <div className={classes.container}>
-      <div className={classes.bg}>
-        <div />
-      </div>
-      <div className={`${classes.topBar} ${scrollY > 30 ? classes.active : ""}`}>
-        <div className={classes.topLogo}>
-          <img src="/assets/linktree/logo.png" layout="fill" alt="logo" />
+    <div className={classes.pageOverlay}>
+      <div className={classes.container}>
+        <div className={classes.bg}>
+          <div />
         </div>
-        <div className={classes.headerTop}>💧GenaDrop💧</div>
-        <button className={classes.shareBtn} onClick={shareHanlder} type="button">
-          <Share />
-        </button>
-      </div>
-      <div className={classes.wrapper}>
-        <div className={classes.header}>
-          <div className={classes.logo}>
+        <div className={`${classes.topBar} ${scrollY > 30 ? classes.active : ""}`}>
+          <div className={classes.topLogo}>
             <img src="/assets/linktree/logo.png" layout="fill" alt="logo" />
           </div>
-          <div className={classes.title}>💧GenaDrop💧</div>
-          <div className={classes.description}>
-            {" "}
-            The no-code , multi-chain generative art nft creator, minter, & marketplace
+          <div className={classes.headerTop}>💧GenaDrop💧</div>
+          <button className={classes.shareBtn} onClick={shareHanlder} type="button">
+            <Share />
+          </button>
+        </div>
+        <div className={classes.wrapper}>
+          <div className={classes.header}>
+            <div className={classes.logo}>
+              <img src="/assets/linktree/logo.png" layout="fill" alt="logo" />
+            </div>
+            <div className={classes.title}>💧GenaDrop💧</div>
+            <div className={classes.description}>
+              {" "}
+              The no-code , multi-chain generative art nft creator, minter, & marketplace
+            </div>
           </div>
-        </div>
-        <div className={classes.links}>
-          {links.map((link) => (
-            <a href={link.url} target="_blank" className={classes.link} key={link.title} rel="noreferrer">
-              <img src={link.img} alt={link.title} />
-              <div>{link.title}</div>
-            </a>
-          ))}
-        </div>
+          <div className={classes.links}>
+            {links.map((link) => (
+              <a href={link.url} target="_blank" className={classes.link} key={link.title} rel="noreferrer">
+                <img src={link.img} alt={link.title} />
+                <div>{link.title}</div>
+              </a>
+            ))}
+          </div>
 
-        {/* <div className={classes.copyright}>© Copyright by {new Date().getFullYear()}&nbsp; Banyan Collective</div> */}
+          {/* <div className={classes.copyright}>© Copyright by {new Date().getFullYear()}&nbsp; Banyan Collective</div> */}
+        </div>
       </div>
     </div>
   );
