d7c029b60f8e1691fd71921c6d78e921e8a7440f
nearbuilder <rabea9220@gmail.com>
2022-09-16 15:43:55 +0200

    organize webcam code

--- src/components/mint-webcam/Capture/Capture.jsx
+++ src/components/mint-webcam/Capture/Capture.jsx
@@ -1,50 +1,19 @@
 import React, { useRef, useState, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useLongPress } from "use-long-press";
-import axios from "axios";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-// import Webcam from "react-webcam";
 import { Camera } from "./Camera";
 import classes from "./Capture.module.css";
 import useTimer from "./useTimer";
 import WebcamEnable from "../webcam-enable/webcamEnable";
+import RecordBtn from "./RecordBtn";
+import { getFileFromBase64, takePicture, switchCameraToRear, updateVideoSize, generateGif } from "./Capture-script";
 // icons
 import { ReactComponent as IconCapture } from "../../../assets/capture-btn.svg";
 import { ReactComponent as CameraSwitch } from "../../../assets/camera-switch.svg";
 import { ReactComponent as ArrowLeft } from "../../../assets/arrow-left-stretched.svg";
 import { ReactComponent as CloseIcon } from "../../../assets/icon-close.svg";
 import { GenContext } from "../../../gen-state/gen.context";
-import { setZip, setLoader, setNotification } from "../../../gen-state/gen.actions";
-
-// record button
-const RecordBtn = ({ seconds }) => (
-  <div className={classes.RecordBtnWrapper}>
-    <CircularProgressbar
-      value={seconds / 500}
-      maxValue={1}
-      strokeWidth={12}
-      styles={buildStyles({
-        // Rotation of path and trail, in number of turns (0-1)
-        rotation: 0.25,
-
-        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-        strokeLinecap: "round",
-
-        // Text size
-        textSize: "16px",
-
-        // How long animation takes to go from one percentage to another, in seconds
-        pathTransitionDuration: 0.5,
-
-        // Colors
-        pathColor: "#FF3236",
-        trailColor: "#ffffff",
-      })}
-    />
-    <div className={classes.recordBtn} />
-  </div>
-);
+import { setZip } from "../../../gen-state/gen.actions";
 
 const Capture = () => {
   const history = useHistory();
@@ -79,20 +48,17 @@ const Capture = () => {
   // video capture
   const [recordedChunks, setRecordedChunks] = useState([]);
   const [capturing, setCapturing] = useState(false);
-  const [numberOfCameras, setNumberOfCameras] = useState(0);
+  const [, setNumberOfCameras] = useState(0);
   const {
     toggle,
     img,
     webcam,
-    width,
-    height,
     gif,
     video,
     currenFile,
     activeFile,
     gifGenrating,
     webcamCurrentType,
-    trackRecord,
     videoDuration,
     imgList,
   } = state;
@@ -101,126 +67,13 @@ const Capture = () => {
     setState((state) => ({ ...state, ...payload }));
   };
   const { dispatch } = useContext(GenContext);
-  // update the video to fit different screen
-  const isLandscape = height <= width;
-  const ratio = isLandscape ? width / height : height / width;
-  const videoConstraints = {
-    facingMode: webcam,
-    width,
-    height,
-    // ...(ratio && { ratio }),
-  };
-  const updateVideoSize = () => {
-    const newWidth = webcamWrapper.current?.clientWidth;
-    const newHeight = webcamWrapper.current?.clientHeight;
-    handleSetState({ height: newHeight, width: newWidth });
-  };
+
   useEffect(() => {
-    updateVideoSize();
-    window.addEventListener("resize", updateVideoSize);
+    updateVideoSize(webcamWrapper, handleSetState);
+    window.addEventListener("resize", () => updateVideoSize(webcamWrapper, handleSetState));
   }, [webcamWrapper.current?.clientWidth]);
 
   useEffect(() => {}, [webcamRef, capturing]);
-
-  // switch camera from front to rear for mobile view
-  const switchCameraToRear = () => {
-    const webcamStatus = webcam === "user" ? "environment" : "user";
-    handleSetState({ webcam: webcamStatus });
-    webcamRef.current.switchCamera();
-  };
-
-  // enable webcam
-  const getVideo = () => {
-    handleSetState({
-      toggle: true,
-    });
-  };
-  const cancel = () => {
-    history.push("/mint/1of1");
-  };
-
-  // Picture Handler
-  const takePicture = () => {
-    const imageSrc = webcamRef.current.takePhoto();
-    console.log(imageSrc);
-    handleSetState({ img: imageSrc });
-  };
-  const downloadImg = () => {
-    const ImageBase64 = img.split("data:image/png;base64,")[1];
-    const a = document.createElement("a"); // Create <a>
-    a.href = `data:image/png;base64,${ImageBase64}`; // Image Base64 Goes here
-    a.download = "Image.png"; // File name Here
-    a.click(); // Downloaded file
-  };
-  function getFileFromBytes(string64, fileName, type) {
-    // const trimmedString = string64.split(",")[1];
-    // const imageContent = atob(trimmedString);
-    const buffer = new ArrayBuffer(string64.length);
-    const view = new Uint8Array(buffer);
-
-    for (let n = 0; n < string64.length; n++) {
-      view[n] = string64.charCodeAt(n);
-    }
-    const blob = new Blob([buffer], { type });
-    return new File([blob], fileName, { lastModified: new Date().getTime(), type });
-  }
-  function getFileFromBase64(string64, fileName, type) {
-    const trimmedString = string64.split(",")[1];
-    const imageContent = atob(trimmedString);
-    const buffer = new ArrayBuffer(imageContent.length);
-    const view = new Uint8Array(buffer);
-    for (let n = 0; n < imageContent.length; n++) {
-      view[n] = imageContent.charCodeAt(n);
-    }
-    const blob = new Blob([buffer], { type });
-    return new File([blob], fileName, { lastModified: new Date().getTime(), type });
-  }
-
-  // Genrate GIF
-  async function getBase64(file) {
-    // console.log(duration);
-    dispatch(setLoader("Generating GIF"));
-    handleSetState({
-      gifGenrating: true,
-    });
-    // const reader = new FileReader();
-    // reader.readAsDataURL(file);
-    // reader.onload = function () {
-    axios
-      .post("https://phantaminum.pythonanywhere.com/gif", {
-        urls: imgList,
-        duration: videoDuration / 200,
-      })
-      .then((res) => {
-        const gifFile = getFileFromBytes(res.data.data, "Image.gif", "image/gif");
-        handleSetState({ gif: gifFile, currenFile: gifFile });
-
-        dispatch(setLoader(""));
-        dispatch(
-          setNotification({
-            message: "GIF generated successfully",
-            type: "success",
-          })
-        );
-        handleSetState({
-          gifGenrating: false,
-        });
-        return res.data.data;
-      })
-      .catch(() => {
-        dispatch(setLoader(""));
-        dispatch(
-          setNotification({
-            message: `Something Went Wrong, Please Try Again`,
-            type: "error",
-          })
-        );
-      });
-    // };
-    // reader.onerror = function (error) {
-    //   console.log("Error: ", error);
-    // };
-  }
 
   // stopwatch, set to 8 sec
   const { seconds, start, stop } = useTimer();
@@ -256,9 +109,6 @@ const Capture = () => {
           console.error({ err2 });
         }
       }
-      // mediaRecorderRef.current = new MediaRecorder(webcamRef.current.getStream(), {
-      //   mimeType: "video/webm",
-      // });
       mediaRecorderRef.current.addEventListener("dataavailable", handleDataAvailable);
       mediaRecorderRef.current.start();
     },
@@ -272,7 +122,7 @@ const Capture = () => {
         type,
       });
       const file = new File([blob], "video.mp4", { lastModified: new Date().getTime(), type });
-      getBase64(file);
+      generateGif(handleSetState, dispatch, imgList, videoDuration);
       handleSetState({ video: file, currenFile: file });
       setRecordedChunks([]);
     }
@@ -297,18 +147,6 @@ const Capture = () => {
     });
     handleStopCaptureClick();
   };
-  // const startCapture = () => {
-  //   start();
-  //   console.log("start");
-  //   // while (seconds < 500) {
-  //   // const imageSrc = webcamRef.current.takePhoto();
-  //   console.log(seconds);
-  //   // handleSetState({
-  //   //   imgList: [...imgList, imageSrc],
-  //   // });
-  //   // }
-  //   if (seconds) stop();
-  // };
 
   useEffect(() => {
     if (seconds !== 0 && webcamRef.current) {
@@ -365,53 +203,17 @@ const Capture = () => {
 
     history.push("/mint/1of1");
   };
-  console.log(seconds);
-  // file type switch
-  // useEffect(() => {
-  //   if (activeFile === "gif") {
-  //     if (gif) {
-  //       handleSetState({ currenFile: gif });
-  //     } else {
-  //       getBase64(currenFile);s
-  //     }
-  //   } else if (activeFile === "mp4") {
-  //     handleSetState({
-  //       currenFile: video,
-  //     });
-  //   }
-  // }, [activeFile]);
 
-  // const capture = () => {
-  //   const v = videoRef.current;
-  //   const { duration } = v;
-  //   const totalSecond = parseInt(duration, 10);
-  //   Array(totalSecond + 1)
-  //     .fill(null)
-  //     .forEach((_, index) => {
-  //       setTimeout(() => {
-  //         v.currentTime = index;
-  //         const canvasRef = document.createElement("canvas");
-  //         canvasRef.width = videoRef.current.videoWidth;
-  //         canvasRef.height = videoRef.current.videoHeight;
-  //         canvasRef
-  //           .getContext("2d")
-  //           .drawImage(videoRef.current, 0, 0, videoRef.current.videoWidth, videoRef.current.videoHeight);
-  //         const newCanvas = document.createElement("canvas");
-  //         const newCtx = newCanvas.getContext("2d");
-  //         newCtx.drawImage(videoRef.current, 0, 0, videoRef.current.videoWidth, videoRef.current.videoHeight);
-  //         console.log("dataUrl", newCanvas.toDataURL());
-  //       }, index * 1000);
-  //     });
-
-  // canvasRef.current.toBlob((blob) => {
-  //   const img = new Image();
-  //   img.setAttribute('crossorigin', 'anonymous');
-  //   img.src = window.URL.createObjectUrl(blob);
-  // })
-  // };
   return (
     <div className={`${classes.container}`}>
-      <WebcamEnable toggle={toggle} getVideo={getVideo} />
+      <WebcamEnable
+        toggle={toggle}
+        getVideo={() =>
+          handleSetState({
+            toggle: true,
+          })
+        }
+      />
 
       {img || gif || video ? (
         <div className={classes.cameraWrapper}>
@@ -438,29 +240,6 @@ const Capture = () => {
             <img className={`${classes.cameraShot}`} src={img} alt="camera-shot" />
           )}
           <div className={classes.imgBtn}>
-            {/* {!img && (
-              <div className={classes.typeSelcet}>
-                <div
-                  onClick={() => handleSetState({ activeFile: "gif" })}
-                  className={activeFile === "gif" && classes.active}
-                >
-                  <div className={classes.rdaioBtn}>
-                    <div />
-                  </div>
-                  <p>GIF</p>
-                </div>
-                <div
-                  onClick={() => handleSetState({ activeFile: "mp4" })}
-                  className={activeFile === "mp4" && classes.active}
-                >
-                  <div className={classes.rdaioBtn}>
-                    <div />
-                  </div>
-                  <p>MP4</p>
-                </div>
-              </div>
-            )} */}
-            {/* <a onClick={downloadImg}>Download photo</a> */}
             <div className={`${classes.mintBtn} ${gifGenrating && classes.disabled}`} onClick={continueToMint}>
               Continue
             </div>
@@ -484,24 +263,11 @@ const Capture = () => {
                 }}
               />
             ) : (
-              // <Webcam
-              //   ref={webcamRef}
-              //   audio={false}
-              //   screenshotFormat="image/png"
-              //   style={{
-              //     objectFit: "cover",
-              //   }}
-              //   minScreenshotHeight={videoConstraints.width}
-              //   minScreenshotWidth={videoConstraints.height}
-              //   width={videoConstraints.width}
-              //   height={videoConstraints.height}
-              //   videoConstraints={videoConstraints}
-              // />
               <div className={classes.videoOFF} />
             )}
             <div className={classes.enableContainer}> </div>
           </div>
-          <div className={classes.closeBtn} onClick={cancel}>
+          <div className={classes.closeBtn} onClick={() => history.push("/mint/1of1")}>
             <CloseIcon />
           </div>
           <div className={classes.btnWrapper}>
@@ -515,22 +281,19 @@ const Capture = () => {
               </div>
             )}
             {webcamCurrentType === "picture" ? (
-              <div onClick={takePicture} className={`${classes.captureBtn} ${classes.active}`}>
+              <div
+                onClick={() => takePicture(webcamRef, handleSetState)}
+                className={`${classes.captureBtn} ${classes.active}`}
+              >
                 <IconCapture />
               </div>
             ) : (
               <div {...bind()} className={`${classes.holdBtn} ${classes.active}`}>
                 <RecordBtn seconds={seconds} webcamCurrentType={webcamCurrentType} />
               </div>
-              // <div
-              //   onClick={takePicture}
-              //   className={`${classes.holdBtn} ${classes.active} ${trackRecord ? classes.recording : ""}`}
-              // >
-              //   <RecordBtn seconds={seconds} />
-              // </div>
             )}
             <div className={classes.uploadBtn}>
-              <CameraSwitch onClick={() => switchCameraToRear()} />
+              <CameraSwitch onClick={() => switchCameraToRear(webcam, handleSetState, webcamRef)} />
             </div>
           </div>
         </div>
