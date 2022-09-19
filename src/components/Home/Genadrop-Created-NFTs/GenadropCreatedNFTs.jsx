84dbf58836ab95a0c7e0fb723939d862d091eda2
weber77 <weberkanga77@gmail.com>
2022-09-19 13:40:51 +0100

    random nfts on homepage

--- src/components/Home/Genadrop-Created-NFTs/GenadropCreatedNFTs.jsx
+++ src/components/Home/Genadrop-Created-NFTs/GenadropCreatedNFTs.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import classes from "./GenadropCreatedNFTs.module.css";
 import NFT1 from "../../../assets/nft-1.png";
 import NFT2 from "../../../assets/nft-2.png";
 import NFT3 from "../../../assets/nft-3.png";
 import NFT4 from "../../../assets/nft-4.png";
 import GenadropCarouselCard from "../../Genadrop-Carousel-Card/GenadropCarouselCard";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { GenContext } from "../../../gen-state/gen.context";
+import { shuffle } from "../../../pages/Marketplace/Marketplace-script";
 
 const cardArr = [
   {
@@ -31,9 +33,13 @@ const GenadropCreatedNFTs = () => {
 
   const [state, setState] = useState({
     cardWidth: 0,
+    singles: [],
   });
 
-  const { cardWidth } = state;
+  const { cardWidth, singles } = state;
+  useEffect(() => {}, []);
+  const { singleAlgoNfts, singleAuroraNfts, singlePolygonNfts, singleCeloNfts } = useContext(GenContext);
+  const singleAlgoNftsArr = Object.values(singleAlgoNfts);
 
   const handleSetState = (payload) => {
     setState((state) => ({ ...state, ...payload }));
@@ -43,7 +49,26 @@ const GenadropCreatedNFTs = () => {
     const cardWidth = cardRef.current && cardRef.current.getBoundingClientRect().width;
     handleSetState({ cardWidth });
   }, []);
+  const history = useHistory();
+  useEffect(() => {
+    let singles = [
+      ...(singleAlgoNftsArr || []),
+      ...(singleAuroraNfts || []),
+      ...(singlePolygonNfts || []),
+      ...(singleCeloNfts || []),
+    ];
+    singles = shuffle(singles);
+    console.log(singles.slice(0, 4));
+    handleSetState({ singles: singles.slice(0, 4) });
+  }, [singleAlgoNfts, singleAuroraNfts, singleCeloNfts, singlePolygonNfts]);
 
+  const handlePreview = (chain, Id) => {
+    if (chain) {
+      history.push(`/marketplace/1of1/${chain}/${Id}`);
+    } else {
+      history.push(`/marketplace/1of1/${Id}`);
+    }
+  };
   return (
     <div className={classes.container}>
       <div className={classes.heading}>
@@ -54,10 +79,14 @@ const GenadropCreatedNFTs = () => {
         <div className={classes.btn}>Create Now</div>
       </Link>
       <GenadropCarouselCard cardWidth={cardWidth} gap={16}>
-        {cardArr.map((card, id) => (
-          <div key={id} ref={cardRef} className={classes.card}>
-            <img src={card.NFT} alt="" />
-            <div className={classes.name}>{card.name}</div>
+        {singles.map((card, id) => (
+          <div onClick={() => handlePreview(card.chain, card.Id)}>
+            <div key={id} ref={cardRef} className={classes.card}>
+              <div className={classes.imgContainer}>
+                <img src={card?.image_url} alt="" />
+              </div>
+              <div className={classes.name}>{card?.name}</div>
+            </div>
           </div>
         ))}
       </GenadropCarouselCard>
