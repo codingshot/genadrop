3c491a28da4f17b8f5f04a1fd7c436e90981a63d
weber77 <weberkanga77@gmail.com>
2022-10-13 00:01:33 +0100

    fix deploy issue

--- src/renderless/store-data/StoreData.jsx
+++ src/renderless/store-data/StoreData.jsx
@@ -1,7 +1,9 @@
+import { BigNumber, utils } from "ethers";
 import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { setImageAction, setLayerAction, setNftLayers } from "../../gen-state/gen.actions";
 import { GenContext } from "../../gen-state/gen.context";
+import { getLatestPrice } from "../../utils/priceFeed";
 import {
   deleteAllTraits,
   deleteTrait,
@@ -38,6 +40,11 @@ const StoreData = () => {
     );
   };
 
+  useEffect(() => {
+    getLatestPrice().then((data) => {
+      console.log(Number(utils.formatUnits(data[3], 8)));
+    });
+  }, []);
   useEffect(() => {
     const { type } = layerAction;
     if (type !== "name") return;
---
+++ src/utils/priceFeed.js
@@ -0,0 +1,61 @@
+import { async } from "@firebase/util";
+import { providers, Contract, BigNumber } from "ethers";
+
+const provider = new providers.JsonRpcProvider("https://mainnet.infura.io/v3/30086941e59240839834dc68944af5d7");
+const aggregatorV3InterfaceABI = [
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "description",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+    name: "getRoundData",
+    outputs: [
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "latestRoundData",
+    outputs: [
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const ETH_USD = process.env.REACT_APP_ETH_USD;
+// const ethPriceFeed = new Contract(ETH_USD, aggregatorV3InterfaceABI, provider)
+
+export const getLatestPrice = async () => {
+  const ethPriceFeed = new Contract(ETH_USD, aggregatorV3InterfaceABI, provider);
+  return await ethPriceFeed.latestRoundData();
+};
