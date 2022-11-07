6a9b817e957e8a8c77d2b89eea9b931df7a9b890
weber77 <weberkanga77@gmail.com>
2022-11-07 15:50:13 +0100

    cancel mobile location

--- src/components/Mint/minter/minter.jsx
+++ src/components/Mint/minter/minter.jsx
@@ -500,6 +500,11 @@ const Minter = () => {
   }
 
   const getLocation = () => navigator.geolocation.getCurrentPosition(success, error, options);
+  const details = navigator?.userAgent;
+
+  const regexp = /android|iphone|kindle|ipad/i;
+
+  const isMobileDevice = regexp.test(details);
 
   const enableAccess = () => {
     if (isMobileDevice) {
@@ -532,12 +537,6 @@ const Minter = () => {
 
     getLocation();
   };
-
-  const details = navigator?.userAgent;
-
-  const regexp = /android|iphone|kindle|ipad/i;
-
-  const isMobileDevice = regexp.test(details);
 
   // *************** GET CURRENT LOCATION: END ***************
 
