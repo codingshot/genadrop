791ef625e88625aef5581e293a90e471803f3689
weber77 <weberkanga77@gmail.com>
2022-11-23 15:49:28 +0100

    set tweet coming false

--- src/pages/creating/Creating-script.js
+++ src/pages/creating/Creating-script.js
@@ -76,17 +76,17 @@ const cards = [
     comingSoon: "",
   },
   {
-    title: "AI",
-    description: "Create Digital art with AI",
-    icon: <Ai />,
-    url: "/mint/ai",
-    comingSoon: true,
-  },
-  {
     title: "Tweets",
     description: "Mint your favorite tweets on chain",
     icon: <Tweets />,
     url: "/mint/tweet",
+    comingSoon: "",
+  },
+  {
+    title: "AI",
+    description: "Create Digital art with AI",
+    icon: <Ai />,
+    url: "/mint/ai",
     comingSoon: true,
   },
 ];
