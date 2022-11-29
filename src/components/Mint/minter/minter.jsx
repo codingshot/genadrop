0dd17980aca4bef657d4335225cdceb1f7eb9df0
weber77 <weberkanga77@gmail.com>
2022-11-30 00:20:49 +0100

    remove returns

--- src/components/Mint/minter/minter.jsx
+++ src/components/Mint/minter/minter.jsx
@@ -356,11 +356,9 @@ const Minter = () => {
     }
 
     if (tweet) {
-      // singleMintProps.file = await htmlToImage.toBlob(tweetRef.current);
+      singleMintProps.file = await htmlToImage.toBlob(tweetRef.current);
     }
 
-    console.log(attributes);
-    return;
     if (!(window.localStorage.walletconnect || chainId)) return initConnectWallet({ dispatch });
 
     if (!chainId) {
