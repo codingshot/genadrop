f4ce987e45f5a302d1c8ad3be0e5acbdadadeffc
Jiku Godwill Nsanwi <65053264+Jikugodwill@users.noreply.github.com>
2022-12-13 20:41:11 +0100

    Created AiMint page and added to the routing

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ import CollectionOptions from "./pages/collection-options/CollectionOptions";
 import MarketplaceAll from "./pages/marketplace-all/MarketplaceAll";
 import Creating from "./pages/creating/Creating";
 import Redirect from "./utils/redirect";
-import AI from "./components/Mint/ai/AI";
+import AiMint from "./pages/ai/AiMint";
 
 function App() {
   const [showWelcomeScreen, setShowWelcomeScreen] = useState(true);
@@ -75,7 +75,7 @@ function App() {
               path={["/mint/camera", "/mint/vibe", "/mint/sesh", "/mint/doubletake", "/mint/video"]}
               component={Camera}
             />
-            <Route exact path="/mint/ai" component={AI} />
+            <Route exact path="/mint/ai" component={AiMint} />
             <Route exact path="/mint/:mintId" component={CollectionToSingleMinter} />
             <Route exact path="/mint/:mintId/minter" component={Minter} />
             <Route exact path="/marketplace" component={Marketplace} />
