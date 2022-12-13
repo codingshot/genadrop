8bc76b29e3680ff409008c53f1c4098d46a39301
Jiku Godwill Nsanwi <65053264+Jikugodwill@users.noreply.github.com>
2022-12-13 21:00:18 +0100

    Renamed and repositions AI files

---
+++ src/components/Mint/ai/AI.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./ai.module.css";
+
+const AI = () => {
+  return <div>AI</div>;
+};
+
+export default AI;
--- src/pages/ai/AiMint.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AiMint = () => {
-  return <div>AiMint</div>;
-};
-
-export default AiMint;
