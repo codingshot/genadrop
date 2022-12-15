a75ab5d4a3502a253cce881d224d1ecf9588afc8
Jiku Godwill Nsanwi <65053264+Jikugodwill@users.noreply.github.com>
2022-12-15 18:51:07 +0100

    Added the svgs for the download and reload icons

--- src/components/Mint/ai/AI.jsx
+++ src/components/Mint/ai/AI.jsx
@@ -1,24 +1,92 @@
+/* eslint-disable no-use-before-define */
 import React from "react";
 import { useState } from "react";
 import classes from "./ai.module.css";
+import { ReactComponent as Download } from "../../../assets/mint-ai-page/download-simple.svg";
+import { ReactComponent as Reload } from "../../../assets/mint-ai-page/icon-reload.svg";
+import { async } from "regenerator-runtime";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const AI = () => {
   const [wordCount, setWordCount] = useState(0);
+  const [imageDimension, setImageDimension] = useState(256);
+  const [downloadStatus, setDownloadStatus] = useState(false);
+  const [promptText, setPromptText] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const handleAiDesc = (e) => {
-    // e.target.value.length;
-    // console.log({ value: e.target.value, text: String(e.target.value).length });
+    setPromptText(e.target.value);
     setWordCount(String(e.target.value).trim().length);
   };
+
+  const imageDimensionChangeHandler = (e) => {
+    setImageDimension(e.target.value.trim());
+  };
+
+  const imageDownloadHandler = (e) => {
+    e.preventDefault();
+    setDownloadStatus(true);
+  };
+
+  // eslint-disable-next-line no-shadow
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.table({ prompt: promptText, size: imageDimension });
+    generateIamgeRequest();
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: promptText,
+      n: 1,
+      size: String(`${imageDimension}x${imageDimension}`),
+    }),
+  };
+
+  const generateIamgeRequest = async () => {
+    try {
+      fetch("https://twitter-api-weber77.vercel.app/genImage", requestOptions).then(async (response) => {
+        // if (!response.ok) {
+        //   throw new Error("That image could not be generated");
+        // }
+
+        const data = await response.json();
+        console.log(data.data.data[0].url);
+
+        setImageUrl(data.data.data[0].url);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    generateIamgeRequest();
+  }, []);
+
   return (
     <main className={classes.aiMain}>
       <section className={classes.wrapper || classes.aiLeft}>
-        <form className={classes.promptForm}>
+        <form className={classes.promptForm} onSubmit={formSubmitHandler}>
           <div className={classes.promptFormTop}>
             <h2 className={classes.promtFormTitle}>Enter prompt</h2>
             <span className={classes.descWordCount}>{wordCount}/200</span>
           </div>
-          <input type="text" className={`${classes.wrapper} ${classes.aiTextInput}`} onChange={handleAiDesc} />
+          <input
+            type="text"
+            className={`${classes.wrapper} ${classes.aiTextInput}`}
+            onChange={handleAiDesc}
+            value={promptText}
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus
+          />
           <section className={classes.artStyleSection}>
             <h2 className={classes.artStyle}>Art Style</h2>
             <main className={classes.artStyleList}></main>
@@ -35,18 +103,33 @@ const AI = () => {
             <label htmlFor="height" className={classes.sizeLabel}>
               <span>Height</span>
               <span className={classes.sizeInWrapper}>
-                <input type="text" className={classes.sizeInput} name="height" /> <span>px</span>
+                <input type="text" className={classes.sizeInput} name="height" value={imageDimension} /> <span>px</span>
               </span>
             </label>
             <label htmlFor="width" className={classes.sizeLabel}>
               <span>Width</span>
               <span className={classes.sizeInWrapper}>
-                <input type="text" className={classes.sizeInput} name="width" /> <span>px</span>
+                <input
+                  type="text"
+                  value={imageDimension}
+                  onChange={imageDimensionChangeHandler}
+                  className={classes.sizeInput}
+                  name="width"
+                />{" "}
+                <span>px</span>
               </span>
             </label>
           </div>
         </section>
-        <output className={classes.artPreview} />
+        <output className={classes.artPreview} style={{ backgroundImage: `url(${imageUrl})` }} />
+        <button
+          type="submit"
+          className={`${classes.wrapper} ${classes.imageDownloadBtn}`}
+          onClick={imageDownloadHandler}
+        >
+          {downloadStatus ? <Reload /> : <Download />}
+          <span>Download</span>
+        </button>
       </form>
     </main>
   );
