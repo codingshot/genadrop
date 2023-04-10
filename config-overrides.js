5dd8208cb900614ccaaa63bb3bf8b4108ff6352c
aminudin <amin4udin@gmail.com>
2023-04-11 04:32:23 +0700

    BugFix: adding more obfuscate on webpack

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const CryptoJS = require('crypto-js');
 const WebpackObfuscator = require('webpack-obfuscator');
+const crypto = require('crypto');
 
 const encryptionKey = 'testesttest$testtest'; // Letakkan kunci enkripsi Anda di environment variable
 
@@ -20,8 +21,34 @@ module.exports = function override(config, env) {
   config.plugins.push(
     new WebpackObfuscator({
       rotateStringArray: true,
+      stringArray: true,
+      stringArrayEncoding: ['base64', 'rc4'],
+      stringArrayThreshold: 0.75,
+      obfuscationSeed: crypto.randomBytes(32),
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.75,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 0.4,
       debugProtection: true,
-      debugProtectionInterval: 1000, // misalnya, setiap 1 detik
+      debugProtectionInterval: 1000,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: 'hexadecimal',
+      log: false,
+      renameGlobals: true,
+      reservedNames: [],
+      rotateStringArray: true,
+      selfDefending: true,
+      shuffleStringArray: true,
+      simplify: true,
+      splitStrings: false,
+      splitStringsChunkLength: 10,
+      stringArrayIndexShift: true,
+      stringArrayWrappersCount: 1,
+      stringArrayWrappersChainedCalls: true,
+      stringArrayWrappersType: 'function',
+      target: 'browser',
+      transformObjectKeys: true,
+      unicodeEscapeSequence: false,
     })
   );
 
