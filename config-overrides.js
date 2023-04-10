62b1f44d255e504237d91a8edcd9487cf85af518
aminudin <amin4udin@gmail.com>
2023-04-11 04:03:12 +0700

    BugFix: adding more obfuscate on webpack

--- config-overrides.js
+++ config-overrides.js
@@ -20,33 +20,8 @@ module.exports = function override(config, env) {
   config.plugins.push(
     new WebpackObfuscator({
       rotateStringArray: true,
-      stringArray: true,
-      stringArrayEncoding: ['base64', 'rc4'],
-      stringArrayThreshold: 0.75,
-      obfuscationSeed: CryptoJS.enc.Utf8.parse(encryptionKey + Math.random().toString(36).substring(2, 15)),
-      controlFlowFlattening: true,
-      controlFlowFlatteningThreshold: 0.75,
-      deadCodeInjection: true,
-      deadCodeInjectionThreshold: 0.4,
       debugProtection: true,
       debugProtectionInterval: 1000, // misalnya, setiap 1 detik
-      disableConsoleOutput: true,
-      identifierNamesGenerator: 'hexadecimal',
-      log: false,
-      renameGlobals: true,
-      reservedNames: [],
-      selfDefending: true,
-      shuffleStringArray: true,
-      simplify: true,
-      splitStrings: false,
-      splitStringsChunkLength: 10,
-      stringArrayIndexShift: true,
-      stringArrayWrappersCount: 1,
-      stringArrayWrappersChainedCalls: true,
-      stringArrayWrappersType: 'function',
-      target: 'browser',
-      transformObjectKeys: true,
-      unicodeEscapeSequence: false,
     })
   );
 
