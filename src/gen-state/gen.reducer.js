8f378b4f9e5b2c0eb143aff7e078a5717377e304
codingshot <45281667+codingshot@users.noreply.github.com>
2024-01-27 07:46:38 -0500

    lol needed a free plan

--- src/gen-state/gen.reducer.js
+++ src/gen-state/gen.reducer.js
@@ -80,7 +80,7 @@ export const INITIAL_STATE = {
   sessions: [],
   toggleSessionModal: false,
   toggleCollectionNameModal: false,
-  currentPlan: "free", // free|noobs|geeks|ogs
+  currentPlan: "ogs", // free|noobs|geeks|ogs
   upgradePlan: false,
   actionProgress: {
     totalCount: 0,
